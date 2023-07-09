5150a4187852566c12999d5b3f9dcacfb45cb1eb
Reynaldo Martínez Reyes <mr026812@gmail.com>
2023-07-09 15:38:00 -0500

    Added required dependencies
    - The main java file was created.
    - Added Jackson library dependency.
    - Added MigLayout library dependency.
    - Added AbsoluteLayout library dependency.
    - Added TimingFramework library dependency.

---
+++ src/main/java/com/rmr/converter/main/App.java
@@ -0,0 +1,11 @@
+package com.rmr.converter.main;
+
+/**
+ *
+ * @author Reynaldo Martínez Reyes
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Starting the Alura converter challenge...");
+    }
+}
