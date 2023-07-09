09b1c80f0b7811cfb6b6d57f55b7e4b935686514
Reynaldo Martínez Reyes <mr026812@gmail.com>
2023-07-09 15:01:52 -0500

    Project creation
    - Added .gitignore file

---
+++ src/main/java/com/rmr/converter/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rmr.converter;
+
+/**
+ *
+ * @author MARTINEZ
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
